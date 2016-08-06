67933f4aa3e585e91f423866914993fd59df9dad
Hsue66 <dufmaakedl@gmail.com>
2016-08-06 10:47:34 +0300

    Editing motor
    Editing motor

--- ZumoBot_Lib.cydsn/Motor.c
+++ ZumoBot_Lib.cydsn/Motor.c
@@ -1,14 +1,14 @@
 /**
  * @file    Motor.c
- * @brief   Motor method file
- * @details 
+ * @brief   Basic methods for operating motor sensor
+ * @details included in Zumo shield
 */
 
 #include "Motor.h"
 
 /**
-* @brief    Start motors
-* @details
+* @brief    Starting motor sensors
+* @details  
 */
 void motor_Start()
 {
@@ -17,7 +17,7 @@ void motor_Start()
 
 
 /**
-* @brief    Stop motors
+* @brief    Stopping motor sensors
 * @details
 */
 void motor_Stop()
@@ -26,8 +26,8 @@ void motor_Stop()
 }
 
 /**
-* @brief    Move motors forward
-* @details  gives same speed to PWM make motor goes forward
+* @brief    Moving motors forward
+* @details  giveing same speed to each side of PWM to make motors go forward
 */
 void motor_forward(uint8 speed,uint32 delay)
 {
@@ -37,7 +37,7 @@ void motor_forward(uint8 speed,uint32 delay)
 }
 
 /**
-* @brief    Move motors turn around
+* @brief    Moving motors to make a turn 
 * @details  moving left when l_speed < r_speed or moving right when l_speed > r_speed
 */
 void motor_turn(uint8 l_speed, uint8 r_speed, uint32 delay)
@@ -49,8 +49,8 @@ void motor_turn(uint8 l_speed, uint8 r_speed, uint32 delay)
 
 
 /**
-* @brief    Move motors backward
-* @details  set backward mode to each motors and gives same speed to PWM
+* @brief    Moving motors backward
+* @details  setting backward mode to each motors and gives same speed to each side of PWM
 */
 void motor_backward(uint8 speed,uint32 delay)
 {
@@ -60,4 +60,3 @@ void motor_backward(uint8 speed,uint32 delay)
     PWM_WriteCompare2(speed); 
     CyDelay(delay);
 }
-/* [] END OF FILE */
