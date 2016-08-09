3a53ead0706610ecbc8f3b8a2f9e0f9c0cd44151
Hsue66 <dufmaakedl@gmail.com>
2016-08-09 10:43:32 +0300

    GyroAccel
    gyro n accel

---
+++ ZumoBot_Lib_GyroAccel.cydsn/Gyro.c
@@ -0,0 +1,22 @@
+/**
+ * @file    Gyro.c
+ * @brief   Gyroscope method file. For more details, please refer to Gyro.h file. 
+ * @details part number: L3GD20H (included in Zumo shield)
+*/
+#include "gyro.h"
+
+/*
+uint16 value_convert_gyro(uint16 raw)            // ****THING TO DO : converting to angle!!!****
+{
+    float rate_result;
+    //float angle_result;
+    float G_GAIN = 0.07;                        // FS = 2000 dps
+    
+  
+    rate_result = (float)raw * G_GAIN;          // degrees per second for Gyroscope
+    //angle_result += rate_result * DT;
+    
+    return rate_result;
+}
+*/
+
---
+++ ZumoBot_Lib_GyroAccel.cydsn/main.c
@@ -0,0 +1,379 @@
+/**
+* @mainpage ZumoBot Project
+* @breif    You can make your own ZumoBot with various sensors.
+* @details  <br>
+    <p>
+    <B>General</B><br>
+    You will use Pololu Zumo Shields for your robot project with CY8CKIT-059(PSoC 5LP) from Cypress semiconductor.This 
+    library has basic methods of various sensors and communications so that you can make what you want with them.
+    <br><br><br>
+    </p>
+    
+    <p>
+    <B>Sensors</B><br>
+    &nbsp;Included: <br>
+        &nbsp;&nbsp;&nbsp;&nbsp;LSM303D: Accelerometer & Magnetometer<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;L3GD20H: Gyroscope<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;Reflectance sensor<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;Motors
+    &nbsp;Wii nunchuck<br>
+    &nbsp;TSOP-2236: IR Receiver<br>
+    &nbsp;HC-SR04: Ultrasonic sensor<br>
+    &nbsp;APDS-9301: Ambient light sensor<br>
+    &nbsp;IR LED <br><br><br>
+    </p>
+    
+    <p>
+    <B>Communication</B><br>
+    I2C, UART, Serial<br>
+    </p>
+*/
+
+#include <project.h>
+#include <stdio.h>
+#include "Motor.h"
+#include "Ultra.h"
+#include "Nunchuk.h"
+#include "Reflectance.h"
+#include "I2C_made.h"
+#include "Gyro.h"
+#include "Accel_magnet.h"
+#include "IR.h"
+#include "Ambient.h"
+
+int rread(void);
+
+
+/*int main()
+{
+    
+}*/
+
+
+/*//ultra sonic sensor//
+int main()
+{
+    CyGlobalIntEnable; 
+    UART_Start();
+    Ultra_Start();                          // Ultra Sonic Start function
+ }   
+//*/
+
+/*//nunchuk//
+int main()
+{
+    CyGlobalIntEnable; 
+    UART_Start();
+  
+    nunchuk_start();
+    nunchuk_init();
+    
+    for(;;)
+    {    
+        nunchuk_read();
+    }
+}   
+//*/
+
+/*//IR receiver//
+int main()
+{
+    CyGlobalIntEnable; 
+    UART_Start();
+    
+    unsigned int IR_val; 
+    
+    for(;;)
+    {
+       IR_val = get_IR();
+       printf("%x\r\n\n",IR_val);
+    }    
+ }   
+//*/
+
+/*//Ambient light sensor//
+int main()
+{
+    CyGlobalIntEnable; 
+    UART_Start();
+    
+    I2C_Start();
+    
+    uint16 value =0;
+    
+    I2C_write(0x29,0x80,0x00);
+    
+    value = I2C_read(0x29,0x80);
+    printf("%x ",value);
+    
+    I2C_write(0x29,0x80,0x03);
+    value = I2C_read(0x29,0x80);
+    printf("%x\r\n",value);
+        
+    value = I2C_read(0x29,0x81);
+    printf("%x\r\n",value);
+    for(;;)
+    {
+        
+        uint8 Data0Low,Data0High,Data1Low,Data1High;
+        Data0Low = I2C_read(0x29,CH0_L);
+        Data0High = I2C_read(0x29,CH0_H);
+        Data1Low = I2C_read(0x29,CH1_L);
+        Data1High = I2C_read(0x29,CH1_H);
+        
+        uint8 CH0, CH1;
+        CH0 = convert_raw(Data0Low,Data0High);
+        CH1 = convert_raw(Data1Low,Data1High);
+
+   //     printf("%d %d %d %d\r\n",Data0Low,Data0High, Data1Low,Data1High);
+   //     printf("%d %d\r\n",CH0,CH1);
+   //        printf("%f\r\n",(float)CH1/CH0);
+        
+   
+        double Ch0 = CH0;
+        double Ch1 = CH1;
+        
+        double data = 0;
+        data = getLux(Ch0,Ch1);
+        printf("%lf\r\n",data);    
+    }    
+ }   
+*/
+
+void value_convert_accel(uint16 X_AXIS, uint16 Y_AXIS, uint16 Z_AXIS, uint16 X_raw, uint16 Y_raw)
+{
+    double AccXangle, AccYangle,CFangleX, CFangleY;
+    float x_rate,y_rate;
+    float G_GAIN = 0.07;
+    float DT = 0.02;
+   
+    x_rate = (float)X_raw * G_GAIN *DT;
+    y_rate = (float)Y_raw * G_GAIN *DT;
+    
+    AccXangle = (float) (atan2(Y_AXIS, Z_AXIS)+M_PI) *180 / M_PI;
+    AccYangle = (float) (atan2(Z_AXIS, X_AXIS)+M_PI) *180 / M_PI;
+    
+    CFangleX =0.98 * (CFangleX+x_rate)+0.02* AccXangle;
+    CFangleY =0.98 * (CFangleY+y_rate)+0.02* AccYangle;
+  //If you want to print out the value  
+  printf("%7.3f %7.3f \r\n", CFangleX, CFangleY);
+}
+
+//*//accelerometer//
+int main()
+{
+    CyGlobalIntEnable; 
+    UART_Start();
+  
+    I2C_Start();
+  
+    uint8 X_L_A, X_H_A, Y_L_A, Y_H_A, Z_L_A, Z_H_A;
+    int16 X_AXIS_A, Y_AXIS_A, Z_AXIS_A;
+    
+    I2C_write(ACCEL_MAG_ADDR, ACCEL_CTRL1_REG, 0x37);           // set accelerometer & magnetometer into active mode
+    I2C_write(ACCEL_MAG_ADDR, ACCEL_CTRL7_REG, 0x22);
+    
+    uint8 X_L_G, X_H_G, Y_L_G, Y_H_G, Z_L_G, Z_H_G;
+    int16 X_AXIS_G, Y_AXIS_G, Z_AXIS_G;
+    
+    I2C_write(GYRO_ADDR, GYRO_CTRL1_REG, 0x0F);             // set gyroscope into active mode
+    I2C_write(GYRO_ADDR, GYRO_CTRL4_REG, 0x30);             // set full scale selection to 2000dps    
+    
+    for(;;)
+    {
+        //print out accelerometer output
+        X_L_A = I2C_read(ACCEL_MAG_ADDR, OUT_X_L_A);
+        X_H_A = I2C_read(ACCEL_MAG_ADDR, OUT_X_H_A);
+        X_AXIS_A = convert_raw(X_L_A, X_H_A);
+        
+        Y_L_A = I2C_read(ACCEL_MAG_ADDR, OUT_Y_L_A);
+        Y_H_A = I2C_read(ACCEL_MAG_ADDR, OUT_Y_H_A);
+        Y_AXIS_A = convert_raw(Y_L_A, Y_H_A);
+        
+        Z_L_A = I2C_read(ACCEL_MAG_ADDR, OUT_Z_L_A);
+        Z_H_A = I2C_read(ACCEL_MAG_ADDR, OUT_Z_H_A);
+        Z_AXIS_A = convert_raw(Z_L_A, Z_H_A);
+        
+        
+        //print out gyroscope output
+        X_L_G = I2C_read(GYRO_ADDR, OUT_X_AXIS_L);
+        X_H_G = I2C_read(GYRO_ADDR, OUT_X_AXIS_H);
+        X_AXIS_G = convert_raw(X_H_G, X_L_G);
+        
+        
+        Y_L_G = I2C_read(GYRO_ADDR, OUT_Y_AXIS_L);
+        Y_H_G = I2C_read(GYRO_ADDR, OUT_Y_AXIS_H);
+        Y_AXIS_G = convert_raw(Y_H_G, Y_L_G);
+        
+        
+        Z_L_G = I2C_read(GYRO_ADDR, OUT_Z_AXIS_L);
+        Z_H_G = I2C_read(GYRO_ADDR, OUT_Z_AXIS_H);
+        Z_AXIS_G = convert_raw(Z_H_G, Z_L_G);
+     
+        
+        //printf("ACCEL: %d %d %d %d %d %d \r\n", X_L_A, X_H_A, Y_L_A, Y_H_A, Z_L_A, Z_H_A);
+        value_convert_accel(X_AXIS_A, Y_AXIS_A, Z_AXIS_A,X_AXIS_G,Y_AXIS_G);
+        
+        CyDelay(20);
+    }
+}   
+//*/
+
+
+/*//reflectance//
+int main()
+{
+    CyGlobalIntEnable; 
+    UART_Start();
+  
+    sensor_isr_StartEx(sensor_isr_handler);
+    
+    Refelctance_Start();
+
+    IR_led_Write(1);
+    for(;;)
+    {
+        reflectance_period();      //print out each period of reflectance sensors
+        reflectance_digital();      //print out 0 or 1 according to results of reflectance period
+        
+        CyDelay(500);
+    }
+}   
+*/
+
+ /* //motor//
+int main()
+{
+    CyGlobalIntEnable; 
+    UART_Start();
+
+    motor_Start();              // motor start
+
+    motor_forward(50,2000);     // moving forward
+    motor_turn(10,50,2000);     // turn
+    motor_turn(50,10,2000);     // turn
+    motor_backward(50,2000);    // movinb backward
+       
+    motor_Stop();               // motor stop
+    
+    for(;;)
+    {
+
+    }
+}
+//*/
+    
+/*//gyroscope//
+int main()
+{
+    CyGlobalIntEnable; 
+    UART_Start();
+  
+    I2C_Start();
+  
+    uint8 X_L_G, X_H_G, Y_L_G, Y_H_G, Z_L_G, Z_H_G;
+    int16 X_AXIS_G, Y_AXIS_G, Z_AXIS_G;
+    
+    I2C_write(GYRO_ADDR, GYRO_CTRL1_REG, 0x0F);             // set gyroscope into active mode
+    I2C_write(GYRO_ADDR, GYRO_CTRL4_REG, 0x30);             // set full scale selection to 2000dps    
+    
+    for(;;)
+    {
+        //print out gyroscope output
+        X_L_G = I2C_read(GYRO_ADDR, OUT_X_AXIS_L);
+        X_H_G = I2C_read(GYRO_ADDR, OUT_X_AXIS_H);
+        X_AXIS_G = convert_raw(X_H_G, X_L_G);
+        
+        
+        Y_L_G = I2C_read(GYRO_ADDR, OUT_Y_AXIS_L);
+        Y_H_G = I2C_read(GYRO_ADDR, OUT_Y_AXIS_H);
+        Y_AXIS_G = convert_raw(Y_H_G, Y_L_G);
+        
+        
+        Z_L_G = I2C_read(GYRO_ADDR, OUT_Z_AXIS_L);
+        Z_H_G = I2C_read(GYRO_ADDR, OUT_Z_AXIS_H);
+        Z_AXIS_G = convert_raw(Z_H_G, Z_L_G);
+     
+        printf("H L : %d %d %d %d %d %d \r\n", X_L_G, X_H_G, Y_L_G, Y_H_G, Z_L_G, Z_H_G);
+        printf("%d %d %d \r\n", X_AXIS_G, Y_AXIS_G, Z_AXIS_G);
+        //printf("%d %d %d \r\n", value_convert_gyro(X_AXIS_G), value_convert_gyro(Y_AXIS_G), value_convert_gyro(Z_AXIS_G));
+        
+       CyDelay(50);
+    }
+  
+}   
+//*/
+
+/*//magnetometer//
+int main()
+{
+    CyGlobalIntEnable; 
+    UART_Start();
+  
+    I2C_Start();
+   
+    uint8 X_L_M, X_H_M, Y_L_M, Y_H_M, Z_L_M, Z_H_M;
+    int16 X_AXIS, Y_AXIS, Z_AXIS;
+    
+    I2C_write(GYRO_ADDR, GYRO_CTRL1_REG, 0x0F);             // set gyroscope into active mode
+    I2C_write(GYRO_ADDR, GYRO_CTRL4_REG, 0x30);             // set full scale selection to 2000dps
+    I2C_write(ACCEL_MAG_ADDR, ACCEL_CTRL1_REG, 0x37);           // set accelerometer & magnetometer into active mode
+    I2C_write(ACCEL_MAG_ADDR, ACCEL_CTRL7_REG, 0x22);
+    
+    
+    for(;;)
+    {
+        X_L_M = I2C_read(ACCEL_MAG_ADDR, OUT_X_L_M);
+        X_H_M = I2C_read(ACCEL_MAG_ADDR, OUT_X_H_M);
+        X_AXIS = convert_raw(X_L_M, X_H_M);
+        
+        Y_L_M = I2C_read(ACCEL_MAG_ADDR, OUT_Y_L_M);
+        Y_H_M = I2C_read(ACCEL_MAG_ADDR, OUT_Y_H_M);
+        Y_AXIS = convert_raw(Y_L_M, Y_H_M);
+        
+        Z_L_M = I2C_read(ACCEL_MAG_ADDR, OUT_Z_L_M);
+        Z_H_M = I2C_read(ACCEL_MAG_ADDR, OUT_Z_H_M);
+        Z_AXIS = convert_raw(Z_L_M, Z_H_M);
+        
+        heading(X_AXIS, Y_AXIS);
+        printf("MAGNET: %d %d %d %d %d %d \r\n", X_L_M, X_H_M, Y_L_M, Y_H_M, Z_L_M, Z_H_M);
+        printf("%d %d %d \r\n", X_AXIS,Y_AXIS, Z_AXIS);
+        CyDelay(50);      
+    }
+}   
+//*/
+
+
+
+#if 0
+int rread(void)
+{
+    SC0_SetDriveMode(PIN_DM_STRONG);
+    SC0_Write(1);
+    CyDelayUs(10);
+    SC0_SetDriveMode(PIN_DM_DIG_HIZ);
+    Timer_1_Start();
+    uint16_t start = Timer_1_ReadCounter();
+    uint16_t end = 0;
+    while(!(Timer_1_ReadStatusRegister() & Timer_1_STATUS_TC)) {
+        if(SC0_Read() == 0 && end == 0) {
+            end = Timer_1_ReadCounter();
+        }
+    }
+    Timer_1_Stop();
+    
+    return (start - end);
+}
+#endif
+
+int _write(int file, char *ptr, int len)
+{
+    (void) file;
+	int n;
+	for(n = 0; n < len; n++) {
+		UART_PutChar(*ptr++);
+	}
+	return len;
+}
+
+/* [] END OF FILE */
